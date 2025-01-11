1400355fa22b48a79fb53915602e99f14d9dab7f
taufik-111 <taufikjamil948@gmail.com>
2025-01-11 23:59:08 +0700

    second commit

--- src/components/front-end/Banner.tsx
+++ src/components/front-end/Banner.tsx
@@ -2,12 +2,13 @@ const Banner = () => {
   return (
     <div className="container mt-32">
       <div className="grid lg:grid-cols-[66%,34%] gap-4 pr-[15px]">
-        <div className="h-[200px] md:h-[260px] bg-[url(/product-banner-1.jpg)] bg-cover bg-center rounded-xl p-8 md:p-16">
+        <div className="h-[200px] md:h-[260px] bg-[url(/product-banner2.jpeg)] bg-cover bg-center rounded-xl p-8 md:p-16">
           <p className="text-topHeadingSecondary text-xl font-medium">
-            Sale 20% off all store
+            
           </p>
           <h2 className="text-topHeadingPrimary font-bold text-xl sm:text-3xl max-w-[240px]">
-            Smartphone BLU G91 Pro 2023
+            BMW R1250 Street Fighters
+            1000 Horse Power
           </h2>
           <a
             className="inline-block mt-6 hover:text-accent text-topHeadingSecondary font-medium"
@@ -16,7 +17,7 @@ const Banner = () => {
             Shop Now
           </a>
         </div>
-        <div className="h-[260px] bg-[url(/product-banner-2.jpg)] bg-right rounded-xl hidden lg:block"></div>
+        <div className="h-[260px] bg-[url(/helmet.png)] bg-right rounded-xl hidden lg:block"></div>
       </div>
     </div>
   );
--- src/components/front-end/Hero.tsx
+++ src/components/front-end/Hero.tsx
@@ -5,16 +5,16 @@ const Hero = () => {
         <div className="flex items-center">
           <div className="max-w-[450px] space-y-4">
             <p className="text-topHeadingSecondary">
-              Starting at <span className="font-bold">$999.00</span>
+              Starting at <span className="font-bold">$399.0</span>
             </p>
 
             <h1 className="text-topHeadingPrimary font-bold text-4xl md:text-5xl">
-              The best note book collection 2023
+              Choose Your Dream Motocycle
             </h1>
 
             <h3 className="text-2xl font-['Oregano',cursive]">
-              Exclusive offer <span className="text-red-600">-10%</span> off
-              this week
+              Machine Guarantee offer <span className="text-red-600">5</span> Years
+      
             </h3>
 
             <a
@@ -27,7 +27,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <img className="ml-auto" src="/hero.png" alt="hero" />
+          <img className="ml-auto" src="/hero.jpeg" alt="hero" height="300" width="300" />
         </div>
       </div>
     </div>
